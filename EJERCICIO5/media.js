6be5c2df6577999b62501e8ba29130b65eb9c70a
SheylaPDV <92029579+SheylaPDV@users.noreply.github.com>
2022-11-07 17:32:40 +0100

    funcion obtener la media

---
+++ EJERCICIO5/media.js
@@ -0,0 +1,14 @@
+// Ejercicio 1, funcion para obtener la media de una lista de numeros.
+
+const media = (list) => {
+  let sum = 0;
+  let i = 0;
+
+  for (var val of list) {
+    sum = sum + val;
+    i++;
+  }
+  const media = sum / i;
+  console.log(media);
+};
+media([8, 9, 12]);
